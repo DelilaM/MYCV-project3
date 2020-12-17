97a949c8c257b5ddd0788ceccac4e43fa5733f43
DelilaM <73423701+DelilaM@users.noreply.github.com>
2020-12-17 18:40:48 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,10 @@
+function openNav(obj) {
+ var obj=document.getElementById(obj);
+  obj.style.display = "block";
+}
+
+
+function closeNav(obj) {
+   var obj=document.getElementById(obj);
+  obj.style.display = "none";
+}
